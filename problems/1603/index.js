5cbe222aed8f7ba33f1212766fb76dc149c19e0b
Derek Caldwell <djohncaldwell@gmail.com>
2023-04-10 15:55:31 -0700

    my 1603 solution

---
+++ problems/1603/index.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.big = big;
+    this.medium = medium;
+    this.small = small;
+
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    if (carType == 1 && this.big > 0){
+        this.big -= 1;
+        return true;
+    } else if (carType == 2 && this.medium > 0){
+        this.medium -= 1;
+        return true;
+    } else if (carType == 3 && this.small > 0){
+        this.small -= 1;
+        return true;
+    } else {
+        return false;
+    }
+};
